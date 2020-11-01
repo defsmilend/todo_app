37139ce0d13379c9a1b4d92fa9f0ca65379400da
defsmilend <ayxand26@gmail.com>
2020-11-01 14:19:12 +0300

    todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  const [newTodo, setNewtodo] = useState('')
+  const [todos, setTodos] = useState([])
+
+
+  function handleNewTodoChange(e) {
+    e.preventDefault();
+    setNewtodo(e.target.value)
+  }
+
+  function handleNewTodo(e) {
+    e.preventDefault();
+    if (newTodo === '') return
+    setTodos([...todos, { id: Date.now(), text: newTodo }])
+    setNewtodo('');
+    e.target.reset()
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="demo-component">
+        <h1>Todos</h1>
+
+        <form onSubmit={handleNewTodo} className="todoform">
+          <div className="todoinput">
+            <input placeholder="Write your todo" value={newTodo} onChange={handleNewTodoChange} />
+          </div>
+          <ul className="todolist">
+            {todos.map((todo) => (
+              <li key={todo.id} className="todo">
+                {todo.text}
+                <a onClick={() => removeTodo(todo.id)}>x</a>
+              </li>
+            ))}
+          </ul>
+        </form>
+      </div>
     </div>
   );
 }
